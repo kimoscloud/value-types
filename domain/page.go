aec86fd5790eb81c681764b32c83714776d83148
nonils <nonils@tutanota.com>
2023-09-22 23:38:46 -0300

    Initial commit

---
+++ domain/page.go
@@ -0,0 +1,61 @@
+package domain
+
+type Page[Entity any] struct {
+	Items      []Entity
+	Total      int
+	PageSize   int
+	PageNumber int
+	TotalPages int
+}
+
+type IPageBuilder[Entity any] interface {
+	SetItems(items []Entity) IPageBuilder[Entity]
+	SetTotal(total int) IPageBuilder[Entity]
+	SetPageSize(pageSize int) IPageBuilder[Entity]
+	SetPageNumber(pageNumber int) IPageBuilder[Entity]
+	SetTotalPages(totalPages int) IPageBuilder[Entity]
+	Build() Page[Entity]
+}
+
+type PageBuilder[Entity any] struct {
+	items      []Entity
+	total      int
+	pageSize   int
+	pageNumber int
+	totalPages int
+}
+
+func (builder *PageBuilder[Entity]) SetItems(items []Entity) IPageBuilder[Entity] {
+	builder.items = items
+	return builder
+}
+
+func (builder *PageBuilder[Entity]) SetTotal(total int) IPageBuilder[Entity] {
+	builder.total = total
+	return builder
+}
+
+func (builder *PageBuilder[Entity]) SetPageSize(pageSize int) IPageBuilder[Entity] {
+	builder.pageSize = pageSize
+	return builder
+}
+
+func (builder *PageBuilder[Entity]) SetPageNumber(pageNumber int) IPageBuilder[Entity] {
+	builder.pageNumber = pageNumber
+	return builder
+}
+
+func (builder *PageBuilder[Entity]) SetTotalPages(totalPages int) IPageBuilder[Entity] {
+	builder.totalPages = totalPages
+	return builder
+}
+
+func (builder *PageBuilder[Entity]) Build() Page[Entity] {
+	return Page[Entity]{
+		Items:      builder.items,
+		Total:      builder.total,
+		PageSize:   builder.pageSize,
+		PageNumber: builder.pageNumber,
+		TotalPages: builder.totalPages,
+	}
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/kimoscloud/value-types
+
+go 1.21.0
